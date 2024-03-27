d17794897cb3d2bce8d6a78df93c685405dde595
skaplan <sinan.kaplan@loodos.com>
2024-03-27 22:43:49 +0300

    Default fonts added, default colors added, Dynamic colors disable.

--- core/designsystem/src/main/java/com/gezenti/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/gezenti/designsystem/theme/Theme.kt
@@ -10,38 +10,49 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val darkColorScheme = darkColorScheme(
+    primary = Color(0xFF19A7CE),
+    onPrimary = Color(0xFF222222),
+    primaryContainer = Color(0xFFF0F1F1), // Başlık ve vurgulu öğelerin arka plan rengi
+    onPrimaryContainer = Color(0xFFDDDDDD), // Başlık ve vurgulu öğelerin arka plan üzerindeki metin rengi
+    secondary = Color(0xFF5296DE), // İkincil öğeler için renk
+    onSecondary = Color(0xFF3B3B3B), // İkincil öğeler üzerindeki metin rengi
+    secondaryContainer = Color(0xFFF6F1F1), // İkincil öğelerin arka plan rengi
+    surface = Color.White, // Yüzey rengi
+    surfaceVariant = Color(0xFFFAFAFA), // Yüzey üzerindeki varyasyon rengi
+    onSurfaceVariant = Color.Black, // Yüzey üzerindeki varyasyon metin rengi
+    background = Color(0xFFF6F1F1), // Ana arka plan rengi
+    tertiary = Color(0xFF146C94),
+    outlineVariant = Color(0xFF888888), // Ana hat varyasyon rengi
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val lightColorScheme = lightColorScheme(
+    primary = Color(0xFF19A7CE),
+    onPrimary = Color(0xFF222222),
+    primaryContainer = Color(0xFFF0F1F1), // Başlık ve vurgulu öğelerin arka plan rengi
+    onPrimaryContainer = Color(0xFFDDDDDD), // Başlık ve vurgulu öğelerin arka plan üzerindeki metin rengi
+    secondary = Color(0xFFFAFAFA), // İkincil öğeler için renk
+    onSecondary = Color(0xFF3B3B3B), // İkincil öğeler üzerindeki metin rengi
+    secondaryContainer = Color(0xFFF6F1F1), // İkincil öğelerin arka plan rengi
+    surface = Color.White, // Yüzey rengi
+    surfaceVariant = Color(0xFFFAFAFA), // Yüzey üzerindeki varyasyon rengi
+    onSurfaceVariant = Color.Black, // Yüzey üzerindeki varyasyon metin rengi
+    background = Color(0xFFF6F1F1), // Ana arka plan rengi
+    tertiary = Color(0xFF146C94),
+    outlineVariant = Color(0xFF888888), // Ana hat varyasyon rengi
 )
 
 @Composable
 fun GezentiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -50,8 +61,8 @@ fun GezentiTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
--- core/designsystem/src/main/java/com/gezenti/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/gezenti/designsystem/theme/Type.kt
@@ -2,33 +2,96 @@ package com.gezenti.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.gezenti.app.core.designsystem.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_bold)),
+        fontWeight = FontWeight.W700,
+        fontSize = 30.sp,
+        lineHeight = 36.sp,
+        letterSpacing = (-1).sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_bold)),
+        fontWeight = FontWeight.W700,
+        fontSize = 20.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_bold)),
+        fontWeight = FontWeight.W700,
+        fontSize = 18.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp,
+    ),
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_light)),
+        fontWeight = FontWeight.W300,
+        fontSize = 18.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    displayMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_light)),
+        fontWeight = FontWeight.W300,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_light)),
+        fontWeight = FontWeight.W300,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+        fontWeight = FontWeight.W500,
+        fontSize = 20.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_regular)),
+        fontWeight = FontWeight.W400,
+        fontSize = 18.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_regular)),
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins_regular)),
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
 )
